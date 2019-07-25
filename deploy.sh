60d79bc0adbb25134c27c98e2f00748fd81d808e
Gillian Middleton <gillian.middleton@sse.com>
2019-07-25 14:23:53 +0100

    git push

--- deploy.sh
+++ deploy.sh
@@ -104,25 +104,29 @@ echo Handling node.js deployment.
 selectNodeVersion
 
 # 2. Install npm packages
-if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
-  cd "$DEPLOYMENT_TARGET"
-  echo "Running $NPM_CMD install --production"
+if [ -e "$DEPLOYMENT_SOURCE/package.json" ]; then
+  cd "$DEPLOYMENT_SOURCE"
   eval $NPM_CMD install --production
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
 fi
 
-# 3. Execute Gulp
-IF EXIST "%DEPLOYMENT_SOURCE%\gulpfile.js" (
-    call .\node_modules\.bin\gulp build
-    IF !ERRORLEVEL! NEQ 0 goto error
-)
+# 3. Run gulp tasks
+if [ -e "$DEPLOYMENT_SOURCE/gulpfile.js" ]; then
+  echo "Running gulp tasks"
+  cd "$DEPLOYMENT_SOURCE"
+  eval './node_modules/.bin/gulp'
+  exitWithMessageOnError "gulp failed"
+  cd - > /dev/null
+fi
+
 
 # 4. KuduSync
 if [[ "$IN_PLACE_DEPLOYMENT" -ne "1" ]]; then
-  "$KUDU_SYNC_CMD" -v 50 -f "$DEPLOYMENT_SOURCE\dist" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh;node_modules;package.json;.gitignore;src"
+  "$KUDU_SYNC_CMD" -v 50 -f "$DEPLOYMENT_SOURCE" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh;node_modules;package.json;gulpfile.js;.gitignore;.htaccess"
   exitWithMessageOnError "Kudu Sync failed"
 fi
+
 
 ##################################################################################################################################
 echo "Finished successfully."
